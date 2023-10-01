47355a94d4dcaa4d18060c24920726ae677e5a53
Courtenay Probert <cprobert@learna.ac.uk>
2023-10-01 21:10:44 +0100

    Moved config classed to separate file

---
+++ src/iron_ssg/config.rs
@@ -0,0 +1,58 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct IronSSGConfig {
+    pub logging: Option<bool>,
+    pub verbose: Option<bool>,
+    pub clean: Option<bool>,
+    pub dist: Option<String>,
+    #[serde(rename = "static_assets")]
+    pub static_assets: Option<Vec<String>>,
+    pub authors: Vec<String>,
+    pub name: String,
+    pub version: String,
+    pub page: Vec<IronSSGPage>,
+}
+
+impl Default for IronSSGConfig {
+    fn default() -> Self {
+        Self {
+            logging: Some(false),
+            verbose: Some(true),
+            clean: Some(false),
+            dist: Some("dist".to_string()),
+            static_assets: None,
+            authors: Vec::new(),
+            name: "IronSSG Website".to_string(),
+            version: "0.1.0".to_string(),
+            page: Vec::new(),
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct IronSSGPage {
+    pub controller: Option<String>,
+    pub path: Option<String>,
+    pub slug: String,
+    pub title: String,
+    pub description: Option<String>,
+    pub view: String,
+    pub components: Option<Vec<String>>,
+    pub model: Option<String>,
+}
+
+impl Default for IronSSGPage {
+    fn default() -> Self {
+        Self {
+            controller: None,
+            path: None,
+            slug: "index".to_string(),
+            title: "".to_string(),
+            description: None,
+            view: "".to_string(),
+            components: None,
+            model: None,
+        }
+    }
+}
--- src/iron_ssg/mod.rs
+++ src/iron_ssg/mod.rs
@@ -1,6 +1,7 @@
 extern crate colored;
 extern crate serde;
 
+pub mod config;
 pub mod errors;
 pub mod file_utils;
 pub mod page_manifest;
@@ -15,69 +16,13 @@ use chrono::{Datelike, Utc};
 use colored::*;
 use handlebars::Handlebars;
 use json::{self, JsonValue};
-use serde::{Deserialize, Serialize};
+
 use serde_json;
 
 // Local modules
+use config::{IronSSGConfig, IronSSGPage};
 use errors::IronSSGError;
 use page_manifest::PageManifest;
-
-#[derive(Deserialize, Serialize, Debug)]
-pub struct IronSSGConfig {
-    pub logging: Option<bool>,
-    pub verbose: Option<bool>,
-    pub clean: Option<bool>,
-    pub dist: Option<String>,
-    #[serde(rename = "static_assets")]
-    pub static_assets: Option<Vec<String>>,
-    pub authors: Vec<String>,
-    pub name: String,
-    pub version: String,
-    pub page: Vec<IronSSGPage>,
-}
-
-impl Default for IronSSGConfig {
-    fn default() -> Self {
-        Self {
-            logging: Some(false),
-            verbose: Some(true),
-            clean: Some(false),
-            dist: Some("dist".to_string()),
-            static_assets: None,
-            authors: Vec::new(),
-            name: "IronSSG Website".to_string(),
-            version: "0.1.0".to_string(),
-            page: Vec::new(),
-        }
-    }
-}
-
-#[derive(Deserialize, Serialize, Clone, Debug)]
-pub struct IronSSGPage {
-    controller: Option<String>,
-    path: Option<String>,
-    slug: String,
-    title: String,
-    description: Option<String>,
-    view: String,
-    components: Option<Vec<String>>,
-    model: Option<String>,
-}
-
-impl Default for IronSSGPage {
-    fn default() -> Self {
-        Self {
-            controller: None,
-            path: None,
-            slug: "index".to_string(),
-            title: "".to_string(),
-            description: None,
-            view: "".to_string(),
-            components: None,
-            model: None,
-        }
-    }
-}
 
 fn log_config(
     config_path: &String,
